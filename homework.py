687c56bb6d480541b72981b5c463df6565e0d934
Dmitriy2311 <118358946+Dmitriy2311@users.noreply.github.com>
2023-03-01 19:49:37 +0300

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,12 +1,12 @@
 import logging
 import os
 import time
+import sys
 import requests
 import telegram
 
 from http import HTTPStatus
 from dotenv import load_dotenv
-from logging.handlers import RotatingFileHandler
 
 load_dotenv()
 
@@ -34,22 +34,21 @@ logger = logging.getLogger(__name__)
 
 def check_tokens():
     """Проверяет токены."""
-    for token in [PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]:
-        if not token:
-            logger.critical(
-                f'Отсутствует обязательная переменная окружения {token}.'
-            )
-            raise SystemExit
+    if all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
+        return True
 
 
 def send_message(bot, message):
     """Отправляет сообщения в чат."""
     try:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
-        logger.debug(f'Сообщение в Telegram отправлено: {message}')
+        logger.debug(
+            f'Сообщение в Telegram отправлено: {message}'
+        )
     except Exception as error:
-        logger.error(f'Ошибка при отправке сообщения {error}')
-        raise Exception(error)
+        logger.error(
+            f'Ошибка при отправке сообщения {error}'
+        )
 
 
 def get_api_answer(current_timestamp):
@@ -57,9 +56,12 @@ def get_api_answer(current_timestamp):
     params = {'from_date': current_timestamp}
     try:
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-    except requests.exceptions.RequestException:
-        raise requests.exceptions.RequestException(
-            'Не удалось получить ответ API'
+    except Exception as error:
+        logging.error(
+            f'Ошибка при запросе к основному API: {error}'
+        )
+        raise Exception(
+            f'Ошибка при запросе к основному API: {error}'
         )
     finally:
         if response.status_code != HTTPStatus.OK:
@@ -73,29 +75,37 @@ def get_api_answer(current_timestamp):
 def check_response(response):
     """Проверяет содержимое ответа от API."""
     if not isinstance(response, dict):
-        error_message = (
+        raise TypeError(
             f'Тип ответа от api не dict. Неверный тип: {type(response)}'
         )
-        raise TypeError(error_message)
     if HOMEWORKS not in response:
-        error_message = 'В ответе от API нет объекта homeworks'
-        raise KeyError(error_message)
-    if not isinstance(response[HOMEWORKS], list):
-        error_message = 'Объект ответа homeworks не является списком'
-        raise TypeError(error_message)
-    return response[HOMEWORKS]
+        raise KeyError(
+            'В ответе от API нет объекта homeworks'
+        )
+    homeworks_list = response[HOMEWORKS]
+    if not isinstance(homeworks_list, list):
+        raise TypeError(
+            'Объект ответа homeworks не является списком'
+        )
+    return homeworks_list
 
 
 def parse_status(homework):
     """Проверяет не изменился ли статус."""
     if HOMEWORK_NAME not in homework:
-        raise KeyError('Ключ homework_name отсутствует в homework')
+        raise KeyError(
+            'Ключ homework_name отсутствует в homework'
+        )
     if STATUS not in homework:
-        raise KeyError('Ключ status отсутствует в homework')
+        raise KeyError(
+            'Ключ status отсутствует в homework'
+        )
     homework_name = homework[HOMEWORK_NAME]
     homework_status = homework[STATUS]
     if homework_status not in HOMEWORK_VERDICTS:
-        raise Exception(f'Неизвестный статус: {homework_status}')
+        raise Exception(
+            f'Неизвестный статус: {homework_status}'
+        )
     verdict = HOMEWORK_VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
@@ -103,43 +113,43 @@ def parse_status(homework):
 def main():
     """Основная логика работы бота."""
     if not check_tokens():
-        bot = telegram.Bot(token=TELEGRAM_TOKEN)
-        current_timestamp = int(time.time())
-        previos_message = ''
+        logger.critical('Нет переменных окружения')
+        sys.exit('Нет переменных окружения')
 
-        while True:
-            try:
-                response = get_api_answer(current_timestamp)
-                homework = check_response(response)[0]
-                message = parse_status(homework)
-                if previos_message != message:
-                    send_message(bot, message)
-                    message = previos_message
-                else:
-                    logging.info('Статус работы не изменился')
-            except Exception as error:
-                message = f'Сбой в работе программы: {error}'
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    current_timestamp = int(time.time())
+    previos_message = ''
+    last_message = ''
+
+    while True:
+        try:
+            response = get_api_answer(current_timestamp)
+            homework = check_response(response)[0]
+            message = parse_status(homework)
+            if previos_message != message:
                 send_message(bot, message)
-            finally:
-                time.sleep(RETRY_PERIOD)
-            logging.info(f'Сообщение {message} отправлено'.format(message))
-    else:
-        error_message = 'Необходимые токены отсутствуют!'
-        logging.error(error_message)
+                message = previos_message
+            else:
+                logging.info('Статус работы не изменился')
+        except Exception as error:
+            message = f'Сбой в работе программы: {error}'
+            if message != last_message:
+                send_message(bot, message)
+                last_message = message
+        finally:
+            time.sleep(RETRY_PERIOD)
+        logging.info(f'Сообщение {message} отправлено'.format(message))
 
 
 if __name__ == '__main__':
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(asctime)s, %(levelname)s, %(message)s,'
-               '%(name)s, %(funcName)s, %(lineno)d'
-    )
-
-    file_handler = RotatingFileHandler(
-        'main.log',
-        mode='a',
-        maxBytes=50000,
-        backupCount=5
-    ), logging.StreamHandler()
-
+               '%(name)s, %(funcName)s, %(lineno)d',
+        handler=[
+            logging.handlers.RotatingFileHandler(
+                f'{os.path.dirname(__file__)}/main.log'
+            ),
+            logging.StreamHandler()
+        ])
     main()
